7c107ffded125b8f1b5f0567565a4ff97652655a
Mohammed Shahed Khan <119789855+Fahadkinq@users.noreply.github.com>
2024-10-17 11:03:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import joblib
-from sklearn.ensemble import GradientBoostingRegressor      
 
 model = joblib.load('xg_boost.pkl')
 st.title('Temperature Prediction(max)')
